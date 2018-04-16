cdeccd74ef45d2cce2579eb30e07af9473cdf8df
Klim <klim1996@gmail.com>
2018-04-16 15:15:27 +0300

    Add Container component

---
+++ src/js/components/Container.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+class Container extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+export default Container;
+
+ReactDOM.render(<Container/>, document.getElementById("container"));
