bf0dff6d53ec3a052f6966a4423ca37fcf39b648
Klim <klim1996@gmail.com>
2018-04-16 15:19:28 +0300

    Add Main component

--- src/js/components/Container.js
+++ src/js/components/Container.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import Main from './Main';
 
 class Container extends Component {
   render() {
     return (
       <div>
+        <Main content="MAIN"/>
       </div>
     );
   }
