25f9f5b0f1514d7706a6c05f8720e37ae1942f6a
Klim <klim1996@gmail.com>
2018-04-30 11:01:15 +0300

    Update Search component

--- src/js/components/Search/Search.js
+++ src/js/components/Search/Search.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Filter from '../Filter/Filter';
+import './Search.scss';
 
 export default class Search extends Component {
   constructor() {
@@ -12,13 +13,15 @@ export default class Search extends Component {
   }
   render () {
     return (
-        <div>
-          <div>FIND YOUR MOVIE</div>
+        <div className="search">
+          <div className="search__title">FIND YOUR MOVIE</div>
 
           <input type="text" ref={(input) => this.input = input}></input>
-          <Filter value={this.props.filter}
-                  setSearchFilter={this.props.setSearchFilter}/>
-          <button onClick={this.search}>Search</button>
+          <div className="search-buttons">
+            <Filter value={this.props.filter}
+                    setSearchFilter={this.props.setSearchFilter}/>
+            <button onClick={this.search}>Search</button>
+          </div>
         </div>
     );
   }
