414d067fb54f17c0631f688b7868ae77fd014ea4
Klim <klim1996@gmail.com>
2018-04-30 13:11:01 +0300

    Add FilmsHeader component

--- src/js/components/Main/Main.js
+++ src/js/components/Main/Main.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import FilmsContainer from '../FilmsContainer/FilmsContainer';
+import FilmsHeader from '../FilmsHeader/FilmsHeader';
 import './Main.scss';
 
 const Main = function(props) {
+  let header;
+
+  if (!props.items.length) {
+    header = '';
+  } else {
+    header = 
+      <div className="row">
+        <FilmsHeader sortBy={props.sortBy}
+                    sortings={props.sortings}
+                    count={props.items.length}
+                    setSorting={props.setSorting}/>
+      </div>;
+  }
+
   return (
-    <main className="row films">
-      
-      <FilmsContainer items={props.items}/>
+    <main className="films">
+      {header}
+      <div className="row">
+        <FilmsContainer items={props.items}/>
+      </div>
     </main>
   );
 };
