b861338f7f84a792122d1d5aaa23d49896b150b3
Klim <klim1996@gmail.com>
2018-04-30 11:00:31 +0300

    Update Footer component

--- src/js/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, {PureComponent} from 'react';
-
-class Footer extends PureComponent {
-  render() {
-    return <footer>{this.props.content}</footer>;
-  }
-}
-
-export default Footer;
