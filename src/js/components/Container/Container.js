7d9053a098f1e8e427bb005a3dbcb100acc5a852
Klim <klim1996@gmail.com>
2018-05-03 20:14:55 +0300

    Move ITEMS in separate file

--- src/js/components/Container/Container.js
+++ src/js/components/Container/Container.js
@@ -5,28 +5,7 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import ErrorBoundarry from '../ErrorBoundary';
 import './Container.scss';
-
-const IMAGE_URL = 'https://ia.media-imdb.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_UX182_CR0,0,182,268_AL_.jpg';
-const ITEMS = [{
-    title: ' Seven Psychopaths',
-    genre: 'Comedy',
-    year: '2007',
-    coverUrl: IMAGE_URL,
-    rating: '8',
-  }, {
-    title: 'Dark Knight',
-    genre: 'Comedy',
-    year: '2007',
-    coverUrl: IMAGE_URL,
-    rating: '9',
-  }, {
-    title: 'The Lord of The Rings',
-    genre: 'Fantasy',
-    year: '2001',
-    coverUrl: IMAGE_URL,
-    rating: '9',
-  }
-];
+import ITEMS from '../../items';
 
 class Container extends Component {
   constructor() {
