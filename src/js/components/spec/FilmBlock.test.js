231a05de2e05f355b136b02b50d20133377947a0
Klim <klim1996@gmail.com>
2018-06-11 01:45:08 +0300

    Test FilmBlock component

---
+++ src/js/components/spec/FilmBlock.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom';
+
+import FilmBlock from '../FilmBlock';
+
+describe('FilmBlock component', () => {
+  it('matches to snapshot', () => {
+    const instance = {
+      id: 711,
+      poster_path: 'path',
+      title: 'someTitle',
+      genre: 'someGenre',
+      year: 'someYear',
+    }
+    const renderedValue = renderer.create(
+      <BrowserRouter>
+        <FilmBlock instance={instance}/>
+      </BrowserRouter>
+    ).toJSON();
+
+    expect(renderedValue).toMatchSnapshot();
+  });
+});
